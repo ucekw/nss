b62a7b0d2e6b1b0ed784f98814ea76d88e24b10a
chethas.lp <lchethas@gmail.com>
2024-11-28 01:13:41 +0530

    FIX: vercel config

--- nsspr/wsgi.py
+++ nsspr/wsgi.py
@@ -12,7 +12,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nsspr.settings')
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'static'))
 app = application
